97d2566d330c9c3614fd63725a26a80e12ff3c51
ujjwalpoudel17 <ujjwalpoudel999.com>
2025-07-02 23:36:39 +0545

    this is day one of python

--- day1.py
+++ day1.py
@@ -3,12 +3,12 @@
 
 
 #variables
-name = "ujjwal"
-age = 23
-address = "gaindakot"
-print ("my name is :",name)
-print ("my age is :",age)
-print ("my address is :",address)
+# name = "ujjwal"
+# age = 23
+# address = "gaindakot"
+# print ("my name is :",name)
+# print ("my age is :",age)
+# print ("my address is :",address)
 
 
 # name = "ujjwal"
@@ -17,7 +17,71 @@ print ("my address is :",address)
 # print(f"my name is {name},i am {age} years old, i live in {address}")
 
 # calculate sum
-a = 5
-b= 10
-sum = a + b
-print ("The value is ",sum)
+# a = 5
+# b= 10
+# sum = a + b
+# print ("The value is ",sum)
+
+# Data types
+# a = "ujjwal"
+# b = 32
+# c = 22.25
+# d = True
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+
+
+
+# operators
+# a = 25
+# b = 35
+# sum = (a + b)
+# diff = (b - a)
+
+# print("thie sum is ",sum)
+# print("the diff is ",diff)
+
+# relational operators
+# a = 30
+# b = 25
+# print(a == b)
+# print(b>a)
+# print(a>b)
+# print(a!=b)
+
+
+
+
+#  assignment operators
+# a = 10
+# a +=10
+# b = 20
+# b -=10
+# print(b)
+# print(a)
+
+
+# logical operators
+# a = True
+# b = False
+# print("and operator :",a and b)
+# a = 10
+# b = 20
+# print("or operator: ",(a == b)  or (b >= a))
+
+
+
+# type converion
+# a = 10
+# a = str(a)
+# print(type(a))
+
+
+# inputs in python
+# name = input("enter your name")
+# print("you entered your name",name)
+
+# a = int(input("enter some values"))
+# print(type(a),a)
