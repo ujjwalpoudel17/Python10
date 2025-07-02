e1bebd66b673ae2b97575f0eb1fbd40a9e5634a1
ujjwalpoudel17 <ujjwalpoudel999.com>
2025-07-02 21:47:25 +0545

    basic pyhton

---
+++ day1.py
@@ -0,0 +1,23 @@
+# #basic
+# print ('ujjwal is learning python')
+
+
+#variables
+name = "ujjwal"
+age = 23
+address = "gaindakot"
+print ("my name is :",name)
+print ("my age is :",age)
+print ("my address is :",address)
+
+
+# name = "ujjwal"
+# age = 23
+# address = "gaindakot"
+# print(f"my name is {name},i am {age} years old, i live in {address}")
+
+# calculate sum
+a = 5
+b= 10
+sum = a + b
+print ("The value is ",sum)
