77194e8e1cca3d62ec9ba73f0b997ef197609015
ujjwalpoudel17 <ujjwalpoudel999.com>
2025-07-08 23:21:46 +0545

    basic pyhton day 4

---
+++ day4.py
@@ -0,0 +1,31 @@
+# dict = {
+#     "name" : "ujjwal",
+#     "address" : "gaindakot",
+#     "age" : "23"
+# }
+# print(dict)
+
+# nested dictionary
+# info = {
+#     "name" : "ujjwal",
+#     "percentage" : "95",
+#     "marks" : {
+#         "mp" : 90,
+#         "np" : 90
+#     }
+# }
+# print(info)
+
+# sets in python
+
+# set = {1,2,3,4}
+# print(set)
+# print(type(set))
+
+
+
+#  intesection and union in set
+# set1 = {1,2,3}
+# set2 = {2,3,4}
+# print(set1.union(set2))
+# print(set1.intersection(set2))
