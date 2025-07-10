13a22a1faa4c9ae419d8b87a0163124bd32e0acd
ujjwalpoudel17 <ujjwalpoudel999.com>
2025-07-10 22:50:51 +0545

    basic pyhton day 5

---
+++ day5.py
@@ -0,0 +1,37 @@
+# loops in python 
+
+# while loop in python
+# count = 1
+# while count <= 5 :
+#     print("Hello")
+#     count += 1
+
+
+# multiplication of 3
+# i = 1
+# while i <= 10:
+#     print(3 * i)
+#     i += 1
+
+
+# print from one to 100
+# i = 1 
+# while i <= 100:
+#     print(i)
+#     i += 1
+
+
+# for loop
+# nums = [1,2,3,4,5]
+# for vals in nums:
+#     print(vals)
+
+
+# range
+# for el in range(5):
+#     print(el)
+
+# seq = range(5)
+   
+# for i in range(2,10):
+#      print(i)
